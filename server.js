473d5cd98c996fcdd7557a87cbc767f5a4b202dd
Redthorn <redthorn57@yahoo.com>
2013-11-24 17:21:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+var jsonObj =
+    {
+          "name" : "John Ryan", "vacation" : 
+          {"places": ["34.40, -199.71","24.67, -78.00","53.35, -6.306"]}
+    };
+
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.json(jsonObj);
 });
 
 var port = process.env.PORT || 5000;
